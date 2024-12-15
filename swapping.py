836c92e6bd2fbe15cd440bc9f26f0d22a6932117
yusragithub <yusrayahya763@gmail.com>
2024-12-15 12:53:44 +0500

    swapping

---
+++ swapping.py
@@ -0,0 +1,5 @@
+number1 = input("Enter your first number ")
+number2 = input("Enter your second number ")
+number3 = input("Enter your third number ")
+number1, number2, number3 = number2, number3, number1
+print(number1, number2, number3)
